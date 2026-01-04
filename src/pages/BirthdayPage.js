8b35196ccd9e077c297abcbee4a3fdcc3c63af22
YashSarag <mathslover3.1415@gmail.com>
2026-01-04 22:41:28 +0530

    Birthday Timer updated Mobile

--- src/pages/BirthdayPage.js
+++ src/pages/BirthdayPage.js
@@ -6,6 +6,7 @@ import d1 from '../assets/decoration/d1.png'
 import d2 from '../assets/decoration/d2.png'
 import d3 from '../assets/decoration/d3.png'
 import d4 from '../assets/decoration/d4.gif'
+import birthday_cake from '../assets/birthday-cake.png'
 
 function BirthdayPage(){
     const {celebrate,setCelebrate,remainingTime,setRemainingTime,calculateRemainingTime} = useContext(CounterContext);
@@ -22,49 +23,55 @@ function BirthdayPage(){
         return ()=> clearInterval(timer)
     })
   return (
-    <div className='flex justify-center items-center w-[91%] max-w-[1160px] mx-auto h-full'>
+    <div className='birthday-bg flex justify-center items-center w-[100%] max-w-[1160px] mx-auto h-full'>
         {
             celebrate ? (<div>
                 <Confetti/>
                 <BirthdayCard/>
             </div>) : (
-                <div className='flex flex-col justify-center items-center'>
-                    <div className='text-center mb-[30px] text-[32px] bg-clip-text text-transparent font-bold h-full timer-caption tracking-[1px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
-                        Not just a timer… It's excitement in numbers!
+                <div className='relative'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className='text-center mb-[30px] text-[32px] bg-clip-text text-transparent font-bold h-full timer-caption tracking-[1px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
+                            Not just a timer… It's excitement in numbers!
+                        </div>
+                        <div className='text-center mb-[30px] text-[32px] bg-clip-text text-transparent font-bold h-full timer-caption-2 hidden tracking-[1px] bg-gradient-to-r from-pink-500 via-red-500 to-orange-400'>
+                            Not just a timer… <br/>It's excitement in numbers!
+                        </div>
+                        <div className='cake-on-way text-center mb-[30px] text-[32px] font-bold h-full timer-caption-3 hidden tracking-[1px] bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
+                            Cake on Way...!
+                        </div>
+                        <div className='birthday-bg-container timer-container grid grid-cols-[1fr,0fr,1fr,0fr,1fr,0fr,1fr] place-items-center fanwall-colors max-w-max px-[70px] py-[50px] rounded-2xl select-none'>
+                            <div className='flex flex-col items-center gap-[10px]'>
+                                <div className='birthday-bg-box bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.days}</div>
+                                <div className='text-slate-100 text-[20px] font-bold'>Days</div>
+                            </div>
+                            <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
+                                <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
+                            </div>
+                            <div className='flex flex-col items-center gap-[10px]'>
+                                <div className='birthday-bg-box bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.hours}</div>
+                                <div className='text-slate-100 text-[20px] font-bold'>Hours</div>
+                            </div>
+                            <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
+                                <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
+                            </div>
+                            <div className='flex flex-col items-center gap-[10px]'>
+                                <div className='birthday-bg-box bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.minutes}</div>
+                                <div className='text-slate-100 text-[20px] font-bold'>Minutes</div>
+                            </div>
+                            <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
+                                <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
+                            </div>
+                            <div className='flex flex-col items-center gap-[10px]'>
+                                <div className='birthday-bg-box bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.seconds}</div>
+                                <div className='text-slate-100 text-[20px] font-bold'>Seconds</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='text-center mb-[30px] text-[32px] bg-clip-text text-transparent font-bold h-full timer-caption-2 hidden tracking-[1px] bg-gradient-to-r from-pink-500 via-red-500 to-orange-400'>
-                        Not just a timer… <br/>It's excitement in numbers!
-                    </div>
-                    <div className='text-center mb-[30px] text-[32px] font-bold h-full timer-caption-3 hidden tracking-[1px] bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
-                        Cake on Way...!
-                    </div>
-                    <div className='timer-container grid grid-cols-[1fr,0fr,1fr,0fr,1fr,0fr,1fr] place-items-center fanwall-colors max-w-max px-[70px] py-[50px] rounded-2xl select-none'>
-                        <div className='flex flex-col items-center gap-[10px]'>
-                            <div className='bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.days}</div>
-                            <div className='text-slate-100 text-[20px] font-bold'>Days</div>
-                        </div>
-                        <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
-                            <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
-                        </div>
-                        <div className='flex flex-col items-center gap-[10px]'>
-                            <div className='bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.hours}</div>
-                            <div className='text-slate-100 text-[20px] font-bold'>Hours</div>
-                        </div>
-                        <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
-                            <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
-                        </div>
-                        <div className='flex flex-col items-center gap-[10px]'>
-                            <div className='bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.minutes}</div>
-                            <div className='text-slate-100 text-[20px] font-bold'>Minutes</div>
-                        </div>
-                        <div className='h-[80px] my-auto space-y-[20px] mx-[20px] timer-separator'>
-                            <div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div><div className='bg-slate-100 w-[8px] h-[8px] rounded-full'></div>
-                        </div>
-                        <div className='flex flex-col items-center gap-[10px]'>
-                            <div className='bg-slate-100 text-[48px] h-[80px] w-[125px] grid place-items-center rounded-md'>{remainingTime.seconds}</div>
-                            <div className='text-slate-100 text-[20px] font-bold'>Seconds</div>
-                        </div>
-                    </div>
+
+                    <img src={birthday_cake}
+                        className='birthday-cake absolute left-[50%] translate-x-[-50%] bottom-[-90px] w-[200px] hidden'
+                    />
                 </div>
             )
         }
